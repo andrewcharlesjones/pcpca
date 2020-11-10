95684ea971d8b63239837783c7b2a97b6fd2c4ab
Andy Jones <ajones788@gmail.com>
2020-11-10 14:42:18 -0500

    new plots

--- experiments/cpca_vary_sample_sizes.py
+++ experiments/cpca_vary_sample_sizes.py
@@ -3,7 +3,7 @@ from scipy.linalg import sqrtm
 
 import sys
 sys.path.append("../models")
-from pcpca import PCPCA
+from cpca import CPCA
 
 
 
@@ -24,7 +24,17 @@ if __name__ == "__main__":
         [2.6, 2.7]
     ]
 
+    
 
+    def abline(slope, intercept):
+        """Plot a line from slope and intercept"""
+        axes = plt.gca()
+        x_vals = np.array(axes.get_xlim())
+        y_vals = intercept + slope * x_vals
+        plt.plot(x_vals, y_vals, '--')
+
+    # Vary gamma and plot what happens
+    # We expect that gamma equal to 0 recovers PCA on X
     n_vals = [200, 200, 20]
     m_vals = [200, 20, 200]
 
@@ -43,37 +53,23 @@ if __name__ == "__main__":
 
         X, Y = X.T, Y.T
 
-        def abline(slope, intercept):
-            """Plot a line from slope and intercept"""
-            axes = plt.gca()
-            x_vals = np.array(axes.get_xlim())
-            y_vals = intercept + slope * x_vals
-            plt.plot(x_vals, y_vals, '--')
-
-        # Vary gamma and plot what happens
-        # We expect that gamma equal to 0 recovers PCA on X
-        
-        
-        
         for ii, gamma in enumerate(gamma_range):
-            # gamma *= m/n
-            pcpca = PCPCA(gamma=gamma, n_components=k)
-            # import ipdb; ipdb.set_trace()
-            pcpca.fit(X, Y)
+            gamma_orig = gamma
+            gamma *= m/n
+            cpca = CPCA(gamma=gamma, n_components=k)
+            cpca.fit(X, Y)
 
             plt.subplot(len(n_vals), len(gamma_range), len(gamma_range) * sample_size_ii + ii+1)
-            plt.title("Gamma = {}".format(gamma))
+            plt.title("Gamma = m/n*{}".format(gamma_orig))
             plt.scatter(X[0, :], X[1, :], alpha=0.5, label="X (target)")
             plt.scatter(Y[0, :], Y[1, :], alpha=0.5, label="Y (background)")
             plt.legend()
             plt.xlim([-7, 7])
             plt.ylim([-7, 7])
             if ii == 0:
-                plt.ylabel("n = {}\nm={}".format(n, m), rotation=0)
+                plt.ylabel("n = {}     \nm={}     ".format(n, m), rotation=0)
 
             origin = np.array([[0], [0]])  # origin point
-            abline(slope=pcpca.W_mle[1, 0] / pcpca.W_mle[0, 0], intercept=0)
-
-    plt.suptitle("Sample size-adjusted gamma")
-    plt.savefig("../plots/pcpca_varying_samplesizes.png")
+            abline(slope=cpca.W[1, 0] / cpca.W[0, 0], intercept=0)
+    plt.savefig("../plots/cpca_varying_samplesizes.png")
     plt.show()
