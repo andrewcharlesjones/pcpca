0c5ad1f0f4366c8e9ab77fc0eecbd5b528860bc9
Andy Jones <ajones788@gmail.com>
2020-12-17 09:36:28 -0500

    packaged

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='pcpca',
+	version='0.1',
+	description='Probabilistic contrastive principal component analysis',
+	url='https://github.com/andrewcharlesjones/pcpca',
+	author='Andy Jones',
+	author_email='ajones788@gmail.com',
+	packages=find_packages())
